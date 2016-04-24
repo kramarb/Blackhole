add1070709542e483489cb3cb45279889575cc4e
Miha Orazem <miha.orazem@gmail.com>
2016-04-24 13:15:57 +0200

    Added assimp as submodule

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,18 @@
 #include <SDL.h>
 
 int main() {
-    if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        std::cout << "SDL init failed" << std::endl;
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        exit(-1);
     }
+    SDL_Window *win = SDL_CreateWindow("BLACKHOLE", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (win == nullptr){
+        std::cerr<< "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    for(;;) {
 
+    }
+    SDL_DestroyWindow(win);
     SDL_Quit();
 }
