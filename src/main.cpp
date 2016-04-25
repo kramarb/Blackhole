9c8e50dc2841ac1ed35b3b780c33a991856038f7
Miha Orazem <miha.orazem@gmail.com>
2016-04-25 09:39:22 +0200

    Working on compilation

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include "GL/glew.h"
 #include <SDL.h>
-
+#include <SDL_opengl.h>
+#include "scene.h"
 int main() {
     if(SDL_Init(SDL_INIT_VIDEO) != 0) {
         exit(-1);
     }
-    SDL_Window *win = SDL_CreateWindow("BLACKHOLE", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window *win = SDL_CreateWindow("BLACKHOLE", 100, 100, 640, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
     if (win == nullptr){
         std::cerr<< "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
-    for(;;) {
+    SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    glewExperimental = GL_TRUE;
+    glewInit();
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
 
-    }
+    printf("%u\n", vertexBuffer);
     SDL_DestroyWindow(win);
     SDL_Quit();
 }
